6a6dca17a8742a538117541c92b50653f3ed88db
Kamila <milka107@interia.pl>
2018-08-15 13:43:55 +0200

    Add action to navigation buttons with animation in jQuery

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    
+    /*For the sticky navigation*/
    $('.js--section-features').waypoint(function(direction){
        if (direction == "down") {
            $('nav').addClass('sticky');
@@ -8,13 +10,32 @@ $(document).ready(function() {
    }, {
        offset: '60px'
    });
-    /*
-    var waypoints = $('#handler-first').waypoint(function(direction) {
-      notify(this.element.id + ' hit 25% from top of window') 
-    }, {
-      offset: '25%'
-    })
-*/
+   
+    /*Scroll on buttons*/
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+    
+    $('.js--scroll-to-start').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+    
+    /*Navigation scroll*/
+    $('#features').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 500);
+    });
+    
+    $('#steps').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-steps').offset().top}, 500);
+    });
+    
+    $('#cities').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-cities').offset().top}, 500);
+    });
+    
+    $('#plans').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 500);
+    });
     
 });
 
